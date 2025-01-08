3ed002c581a623769432e0a4d7931e964893f1b0
dariobrunelli <dariobrunelli95@gmail.com>
2025-01-08 14:18:19 +0100

    version 0.9.9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="clearbox-preprocessor",  
-    version="0.9.8",  
+    version="0.9.9",  
     author="Dario Brunelli",
     author_email="dario@clearbox.ai",
     description="A polars based preprocessor for ML datasets",
