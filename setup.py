7fbe74cd0b2d33560a926f96c0ff0c540e241c84
dariobrunelli <dariobrunelli95@gmail.com>
2025-02-24 11:43:57 +0100

    v0.10.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="clearbox-preprocessor",  
-    version="0.10.1",  
+    version="0.10.2",  
     author="Dario Brunelli",
     author_email="dario@clearbox.ai",
     description="A polars based preprocessor for ML datasets",
