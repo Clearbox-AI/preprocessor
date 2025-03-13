f3b9cfc3ed2658367212d012884ec52a9f14e495
dariobrunelli <dariobrunelli95@gmail.com>
2025-03-13 17:40:31 +0100

    minor fix

--- clearbox_preprocessor/preprocessor.py
+++ clearbox_preprocessor/preprocessor.py
@@ -11,12 +11,14 @@ from typing import List, Tuple, Literal, Dict
 import warnings
 import numpy as np
 
-# from .utils.numerical_transformer import NumericalTransformer
-# from .utils.categorical_transformer import CategoricalTransformer
-# from .utils.datetime_transformer import DatetimeTransformer
-from utils.numerical_transformer import NumericalTransformer
-from utils.categorical_transformer import CategoricalTransformer
-from utils.datetime_transformer import DatetimeTransformer
+from .utils.numerical_transformer import NumericalTransformer
+from .utils.categorical_transformer import CategoricalTransformer
+from .utils.datetime_transformer import DatetimeTransformer
+
+# UNCOMMENT FOR DEBUGGING
+# from utils.numerical_transformer import NumericalTransformer
+# from utils.categorical_transformer import CategoricalTransformer
+# from utils.datetime_transformer import DatetimeTransformer
 
 class Preprocessor:
     ML_TASKS = {"classification", "regression", None}
